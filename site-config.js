0075acf1d87e26fde0bde0972b0d43d7c1a154c5
AutumnRain <amos0927qdy@gmail.com>
2026-05-19 20:23:58 +0800

    Configure GitHub Pages URL

--- site-config.js
+++ site-config.js
@@ -1,9 +1,9 @@
 window.MILK_TEA_SITE_CONFIG = {
   siteName: "我的奶茶版图",
-  siteUrl: "",
+  siteUrl: "https://amos0927qdy-ops.github.io/my-milk-tea-empire/",
   shareTitle: "我的奶茶版图",
   shareDescription: "点亮喝过、想喝和本命奶茶，生成你的奶茶人格分享图。",
-  shareImage: "./assets/china-map-wide.png",
+  shareImage: "https://amos0927qdy-ops.github.io/my-milk-tea-empire/assets/china-map-wide.png",
   studioLabel: "Idea孵化巢",
   studioUrl: "http://127.0.0.1:4180/#project=NN-WEB-004",
   githubLabel: "GitHub",
