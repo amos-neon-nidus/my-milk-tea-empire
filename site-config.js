ef3df6cd125715cc26ffbee8974436f3d25b4b22
AutumnRain <amos0927qdy@gmail.com>
2026-05-19 21:35:21 +0800

    Update Pages URL after repository move

--- site-config.js
+++ site-config.js
@@ -1,9 +1,9 @@
 window.MILK_TEA_SITE_CONFIG = {
   siteName: "我的奶茶版图",
-  siteUrl: "https://amos0927qdy-ops.github.io/my-milk-tea-empire/",
+  siteUrl: "https://amos-neon-nidus.github.io/my-milk-tea-empire/",
   shareTitle: "我的奶茶版图",
   shareDescription: "点亮喝过、想喝和本命奶茶，生成你的奶茶人格分享图。",
-  shareImage: "https://amos0927qdy-ops.github.io/my-milk-tea-empire/assets/china-map-wide.png",
+  shareImage: "https://amos-neon-nidus.github.io/my-milk-tea-empire/assets/china-map-wide.png",
   studioLabel: "Idea孵化巢",
   studioUrl: "",
   githubLabel: "GitHub",
